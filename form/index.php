035cbf0c07a625ddb297ef37d0982255d8a5439c
aashisrjl <aashis252@gmail.com>
2025-01-26 09:14:06 +0545

    form modified

---
+++ form/index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+
+            display: flex;
+            justify-content: center;
+            align-items: center;     
+        }
+    </style>
+</head>
+<body>
+    <!-- what are diffrent html form elements -->
+     <!-- what are differret types of type artributes html 5 -->
+    <form action="submit.php" >
+        <fieldset>
+            <legend>Register Form</legend>
+
+            <div class="section">
+            <label for="username">Username: </label> <br>
+            <input name="username" placeholder="aashis" type="text">
+            </div> <br>
+
+            <div class="section">
+                <label for="email">Email:</label> <br>
+                <input name="email" placeholder="example@gmail.com" type="email">
+                </div> <br>
+
+                <div class="section">
+                    <label for="gender">Gender:</label> <br>
+                    <select name="gender" id="">
+                        <option value="male">male</option>
+                        <option value="fermale">female</option>
+                        <option value="other">other</option>
+                    </select>
+                    </div> <br>
+
+                    <div class="section">
+                    <label for="gender">country:</label> <br>
+                    <input list="conuntry" name="country">
+                    <datalist id="country">
+                        <option value="nepal">nepal</option>
+                        <option value="bhutan">bhutan</option>
+                        <option value="india">india</option>
+                    </datalist>
+                    </div> <br>
+
+                <div class="section">
+                    <label for="password">Password:</label> <br>
+                    <input name="password" placeholder="your password" type="password">
+                    </div> <br>
+                    <button type="submit">submit</button>
+        </fieldset>
+    </form>
+    
+</body>
+</html>
---
+++ form/submit.php
@@ -0,0 +1,9 @@
+<?php
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$gender = $_POST['gender']
+
+die("hello");
+
+?>
